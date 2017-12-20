a42aeabef04461bb0670983d3e55d511e8b1d578
Michał <fierkamichal@gmail.com>
2017-12-20 23:23:44 +0100

    'a'

---
+++ src/WeightedObjectivesMethod.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+/**
+ * Created by Michał on 2017-12-20.
+ */
+public class WeightedObjectivesMethod {
+
+    public void makeF(List<Double[]> list) {
+    }
+}
--- src/simplex.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by Michał on 2017-12-13.
- */
-public class simplex {
-
-    public static void main(String[] args) {
-
-    }
-}
